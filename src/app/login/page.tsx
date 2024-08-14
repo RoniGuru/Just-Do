c02e0b0fe07aee27de569079f126b8c54744170e
Ronit Gurung <lecrepes45@gmail.com>
2024-08-14 12:15:34 +0100

    custom user accounts and sign in button in login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useAuth } from "@clerk/nextjs";
+import { useAuth, SignInButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -14,8 +14,12 @@ export default function LogIn() {
     }
   }, []);
   return (
-    <div className="flex h-96 items-center justify-center text-2xl font-bold text-white">
-      Please Log in{" "}
+    <div className="flex h-full items-center justify-center text-2xl font-bold text-white">
+      <SignInButton>
+        <button className="rounded border border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+          Sign in with Clerk
+        </button>
+      </SignInButton>
     </div>
   );
 }
